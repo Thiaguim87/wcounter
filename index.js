3c3c7a9f3041cee9b4aa2a500e15494adda6ea76
Thiaguim87 <tluiz87@outlook.com>
2023-01-07 21:21:44 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("btn").onclick = function(){
+    var txt = document.getElementById("message").value;
+    var caracteres = document.getElementById("caracteres")
+    var word = document.getElementById("words")
+    if(txt != ""){
+        caracteres.innerHTML = "Characters: " + txt.length;
+        word.innerHTML = "Words: " + txt.split(' ').length;
+    } else{
+        caracteres.innerHTML = "Characters: 0";
+        word.innerHTML = "Words: 0";
+    }
+}
